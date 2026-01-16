fb3e09e4b30c506f8c5134e8a4147c61f184af23
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2026-01-16 15:43:44 -0500

    updated notification setting to include feed details

--- notification.py
+++ notification.py
@@ -42,18 +42,50 @@ class Notification:
         except ValueError:
             return response.text
 
+    def get_feed_id_from_plugin_inst(self, plugin_inst: int) -> int:
+        """Get feed_id from a given plugin instance"""
+        logger.info(f"Fetching feed id for plugin instance with ID: {plugin_inst}")
+        response = self.make_request("GET",f"/plugins/instances/{plugin_inst}/")
+        for item in response:
+            for field in item.get("data", []):
+                if field.get("name") == "feed_id":
+                    return field.get("value")
+        return -1
+
+    def get_feed_details_from_id(self, feed_id: int) -> dict:
+        """Get feed details given a feed id"""
+        feed_details = {}
+
+        logger.info(f"Getting feed details for ID: {feed_id}")
+        response = self.make_request("GET",f"/{feed_id}/")
+        for item in response:
+            for field in item.get("data", []):
+                if field.get("name") == "creation_date":
+                    feed_details["date"] = field.get("value")
+                if field.get("name") == "name":
+                    feed_details["name"] = field.get("value")
+                if field.get("name") == "owner_username":
+                    feed_details["owner"] = field.get("value")
+
+        return feed_details
+
     def run_notification_plugin(self, pv_id: int, msg: str, rcpts: str, smtp: str, search_data: str) -> int:
         """
         Run the pl-notification plugin.
         """
-        email_content = f"Your workflow is now complete. Kindly login to ChRIS for detailed logs."
+        feed_id = self.get_feed_id_from_plugin_inst(pv_id)
+        feed_details = self.get_feed_details_from_id(feed_id)
+        email_content = (f"Your workflow is now complete."
+                         f"\nFeed Name: {feed_details['name']}"
+                         f"\nDate: {feed_details['date']}"
+                         f"\n\nKindly login to ChRIS as *{feed_details['owner']}* to access the logs for more details.")
 
         try:
             plugin_id = self.get_plugin_id({"name": "pl-notification", "version": "0.1.0"})
             instance_id = self.create_plugin_instance(plugin_id, {
                 "previous_id": pv_id,
                 "content": email_content,
-                "title": msg,
+                "title": f"Analysis {feed_details['name']} is complete.",
                 "rcpt": rcpts,
                 "sender": "noreply@fnndsc.org",
                 "mail_server": smtp
