00c98e31451912c2ee110631ef70cca75f66e563
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2026-01-26 13:59:11 -0500

    version bump to 1.1.3

--- dypxFlow.py
+++ dypxFlow.py
@@ -27,7 +27,7 @@ logger_format = (
 logger.remove()
 logger.add(sys.stderr, format=logger_format)
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 DISPLAY_TITLE = r"""
        _           _                ______ _               
